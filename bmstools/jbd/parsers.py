dd8b058c8d972440330200340fde1e9c6778ddc5
Eric Poulsen <eric@zyxod.com>
2020-12-09 20:03:22 -0800

    Variouse bugfixes in the encoders

--- bmstools/jbd/parsers.py
+++ bmstools/jbd/parsers.py
@@ -17,19 +17,26 @@ class BaseParser: pass
 class IntParserX1(BaseParser):
     'encodes or decodes a single integer'
     factor = 1
+
+    @staticmethod
+    def UtoS(v): # unsigned --> signed
+        return struct.unpack('>h', struct.pack('>H',int(v)))[0]
+
+    @staticmethod
+    def StoU(v): # signed --> unsigned
+        return struct.unpack('>H', struct.pack('>h',int(v)))[0]
+
     @classmethod
     def decode(cls, string):
         # fileformat stores int16_t as uint16_t
         # need to convert to signed
-        v = int(string) 
-        v = struct.unpack('>h', struct.pack('>H',v))[0]
+        v = cls.UtoS(string)
         return (v * cls.factor,)
 
     @classmethod
     def encode(cls, values):
-        if type(values) not in (tuple, list):
-            values = (values,)
-        return str(int(values[0] / cls.factor))
+        value = float(values[0])
+        return str(cls.StoU(value / cls.factor))
 
 class IntParserX10(IntParserX1):
     factor = 10
@@ -65,7 +72,7 @@ class Dsgoc2Parser(BaseParser):
     @staticmethod
     def encode(values):
         dsgoc2, dsgoc2_delay = values
-        i = dsgoc2.val | (Dsgoc2DelayEnum.val << 4)
+        i = dsgoc2.val | (dsgoc2_delay.val << 4)
         return str(i)
 
     
@@ -94,8 +101,8 @@ class CxvpDelayParser(BaseParser):
     @staticmethod
     def encode(values):
         covp_high_delay, cuvp_high_delay = values
-        i = (covp_high_delay & 0x3) << 6
-        i |= (cuvp_high_delay & 0x3) << 4
+        i = (covp_high_delay.val & 0x3) << 6
+        i |= (cuvp_high_delay.val & 0x3) << 4
         return str(i)
 
 class DateParser(BaseParser):
@@ -111,13 +118,13 @@ class DateParser(BaseParser):
 
     @staticmethod
     def encode(values):
-        year, month, day = values
+        year, month, day = (int(i) for i in values)
         value = (year - 2000) & 0x7f
         value <<= 4
         value |= month
         value <<= 5
         value |= day
-        return struct.pack('>H', value)
+        return value
 
 class TempParser(IntParserX1):
     @classmethod
@@ -125,10 +132,10 @@ class TempParser(IntParserX1):
         value = int(value)
         return ((value - 2731) / 10,)
 
+    @classmethod
     def encode(cls, values):
-        if type(values) not in (tuple, list):
-            values = (values,)
-        return str(int(values[0] * 10 + 2731))
+        value = int(values[0])
+        return str(int(value * 10 + 2731))
 
 
 class StrParser(BaseParser):
