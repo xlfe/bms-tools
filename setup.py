530caf4674e802076e096fca80e8230e4e38d1db
Eric Poulsen <eric@zyxod.com>
2020-12-27 22:37:31 -0800

    moved genVersionFile into setup.py

--- setup.py
+++ setup.py
@@ -17,23 +17,57 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
+import subprocess
+import argparse
+import sys
+import os
 
-setup(
-    name='bmstools',
-    url='https://gitlab.com/MrSurly/bms-tools.git',
-    maintainer='Eric Poulsen',
-    maintainer_email='"Eric Poulsen" <eric@zyxod.com>',
-    version='0.0.1-dev',
-    install_requires=['pyserial~=3.4', 'openpyxl~=3.0.5', 'numpy==1.19.3'],
+scriptFile = os.path.abspath(__file__)
+scriptDir = os.path.dirname(scriptFile)
 
-    ## numpy version locked because of https://tinyurl.com/y3dm3h86
-    extras_require = {
-        'gui': ['wxPython~=4.1.1', 'pyinstaller']
-    },
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    packages=['bmstools'],
-    entry_points = {
-        'console_scripts': [
-        ]
-    }
-)
+def genVersionFile():
+    cmd = 'git describe --long --dirty --abbrev=10 --tags'.split()
+    version = subprocess.check_output(cmd, cwd = scriptDir).strip()
+    fn = os.path.join(scriptDir, 'bmstools', 'version.py')
+    with open(fn, 'w') as v:
+        v.write(f'''#!/usr/bin/env python
+
+version = {repr(str(version, 'utf-8'))}
+''')
+    return version
+
+
+def doSetup():
+    setup(
+        name='bmstools',
+        url='https://gitlab.com/MrSurly/bms-tools.git',
+        maintainer='Eric Poulsen',
+        maintainer_email='"Eric Poulsen" <eric@zyxod.com>',
+        version='0.0.1-dev',
+        install_requires=['pyserial~=3.4', 'openpyxl~=3.0.5', 'numpy==1.19.3'],
+
+        ## numpy version locked because of https://tinyurl.com/y3dm3h86
+        extras_require = {
+            'gui': ['wxPython~=4.1.1', 'pyinstaller']
+        },
+        license='Creative Commons Attribution-Noncommercial-Share Alike license',
+        packages=['bmstools'],
+        entry_points = {
+            'console_scripts': [
+            ]
+        }
+    )
+
+
+if __name__ == '__main__':
+    p = argparse.ArgumentParser()
+    p.add_argument('--bmstools-gen-version', action = 'store_true')
+
+    n = sys.argv[0]
+    args, rem = p.parse_known_args()
+    sys.argv = [n] + rem
+    ver = genVersionFile()
+    if args.bmstools_gen_version:
+        print(f'version is {ver}')
+    else:
+        doSetup()
