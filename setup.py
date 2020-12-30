9e749f00aee5ddfc153a7dc00220b2d722a470ae
Eric Poulsen <eric@zyxod.com>
2020-12-30 14:52:37 -0800

    fix for `make version`

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ scriptFile = os.path.abspath(__file__)
 scriptDir = os.path.dirname(scriptFile)
 
 def genVersionFile():
-    cmd = 'git describe --long --dirty --abbrev=10 --tags'.split()
+    cmd = 'git describe --dirty --abbrev=10 --tags'.split()
     version = subprocess.check_output(cmd, cwd = scriptDir).strip()
     fn = os.path.join(scriptDir, 'bmstools', 'version.py')
     with open(fn, 'w') as v:
