22451c90a78514bdc18d581730fb8a7b179120a1
Daniel Hofer <daniel_hofer_1@hotmail.com>
2022-02-12 13:05:35 +0100

    add package infos for PyPi

--- setup.py
+++ setup.py
@@ -18,9 +18,6 @@
 
 from setuptools import setup, find_packages
 
-import subprocess
-import argparse
-import sys
 import os
 import versioneer
 
@@ -30,12 +27,16 @@ scriptDir = os.path.dirname(scriptFile)
 setup(
     name='bmstools',
     url='https://gitlab.com/MrSurly/bms-tools.git',
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/bms-tools/bms-tools/-/issues",
+    },
+    author='Eric Poulsen',
+    author_email='"Eric Poulsen" <eric@zyxod.com>',
     maintainer='Eric Poulsen',
     maintainer_email='"Eric Poulsen" <eric@zyxod.com>',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     install_requires=['pyserial~=3.4', 'xlsxwriter==1.3.7'],
-
     extras_require = {
         'gui': ['wxPython~=4.1.1', 'pyinstaller']
     },
@@ -45,6 +46,15 @@ setup(
         'console_scripts': [
             'bmstools_jbd_gui=gui.jbd_gui:main' 
         ]
-    }
+    },
+    description="This package aims to provide open source tools for popular Battery Management Systems (BMS)",
+    long_description="Please see [https://gitlab.com/MrSurly/bms-tools](https://gitlab.com/MrSurly/bms-tools)",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux"
+    ]
 )
 
